59460bf73310249ca3f09f3dba1e915cdc039e52
neha-93 <nkrocks93@gmail.com>
2022-08-04 17:17:48 +0530

    added frontend and stripe integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import axios from './server.js';
+import StripeCheckoutButton from "./stripeCheckoutButton";
+import CurrencyTextField from '@unicef/material-ui-currency-textfield';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+// import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const MyPaymentForm = () => {
+
+  const [value, setValue] = React.useState();
+
+  const [valueRadio, setValueRadio] = React.useState('VISA');
+
+  const [name, setName] = React.useState('Your Name');
+  const [cardNo, setCardNo] = React.useState('');
+  const [expiryDate, setExpiryDate] = React.useState('09/24');
+  const [cvv, setCvv] = React.useState('111');
+  const [address, setAddress] = React.useState('Your Address');
+
+  const [stripePay, setStripePay] = React.useState(false);
+  const [razorPay, setRazorPay] = React.useState(false);
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
   );
-}
 
-export default App;
+  function handleSubmit(e) {
+    console.log(valueRadio);
+    (async () => {
+      const users = await axios.get(`/card/${valueRadio}`);
+      console.log(users);
+      setName(users.data.name);
+      setAddress(users.data.address);
+      setCvv(users.data.cvv);
+      setCardNo(users.data.cardNo);
+      console.log(name);
+      console.log(cardNo);
+      console.log(cvv);
+      console.log(address);
+    })();
+  }
+
+
+  useEffect(()=> {
+    (async () => {
+      const users = await axios.get("/card/RUPAY");
+      console.log(users);
+    })();
+    
+  }, []);
+
+  return (
+  <div>
+
+  <FormControl>
+    <FormLabel id="demo-controlled-radio-buttons-group">Select Card Type</FormLabel>
+    <RadioGroup
+      aria-labelledby="demo-controlled-radio-buttons-group"
+      name="controlled-radio-buttons-group"
+      value={valueRadio}
+      onChange={(event, value)=> setValueRadio(value)}
+    >
+      <FormControlLabel value="VISA" control={<Radio />} label="VISA" />
+      <FormControlLabel value="MASTERCARD" control={<Radio />} label="MasterCard" />
+      <FormControlLabel value="AMERICAN_EXPRESS" control={<Radio />} label="American Express" />
+      <FormControlLabel value="RUPAY" control={<Radio />} label="RuPay" />
+    </RadioGroup>
+  </FormControl>
+
+  <br/>
+
+  <Button variant="contained" type="submit"
+        onClick= {handleSubmit}>Generate</Button>
+
+  <br/>
+
+  <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {valueRadio}
+        </Typography>
+        <Typography variant="h5" component="div">
+        {cardNo}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Name: {name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        CVV: {cvv}
+        </Typography>
+        <Typography variant="body2">
+        Address: {address}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <a onClick={(event, value) => setStripePay(true) && console.log(stripePay)} target="_blank"><Button size="small" >Pay through Stripe gateway</Button></a>
+        <a onClick={(event, value) => setRazorPay(true) && console.log(razorPay)} href="https://rzp.io/l/ePVVGrS2e"><Button size="small">Pay through RazorPay gateway</Button></a>
+      </CardActions>
+    </Card>
+  
+  {stripePay && <div>
+  <h1>Enter amount to be paid.</h1>
+  <CurrencyTextField
+		label="Amount"
+		variant="standard"
+		value={value}
+		currencySymbol="$"
+		//minimumValue="0"
+		outputFormat="string"
+		decimalCharacter="."
+		digitGroupSeparator=","
+		onChange={(event, value)=> setValue(value)}
+    />
+  <StripeCheckoutButton price = {value}/>
+  </div>}
+  </div>
+  )
+};
+
+export default MyPaymentForm;
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8080',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+  });
+
+export default instance;
+
